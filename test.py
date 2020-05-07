59c4f046de5cb112f6fe1e4b885c2e7c927273c0
RémiSimon <remi.simon@eleves.enpc.fr>
2020-05-07 15:38:40 +0200

    Most recent version, with new scores, automatic pull

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import time
 import visualize as vis
 import os
 
-name='Premiere_simulation_1000j'
+name='Simulation_real_prices_2_var'
 this_dir = os.path.dirname(os.path.abspath(__file__))
 t = time.time()
 
@@ -21,10 +21,10 @@ manager.simulate(1000,name)
 loads=np.load(name+"/data_visualize/load_simulation.npy",allow_pickle=True) 
 # keys : players  -- objects : nb_simul*nb_time_steps
 
-bills=np.load(name+"/data_visualize/bill_simulation.npy",allow_pickle=True)
+bills=np.load(name+"/data_visualize/bill_simulation.npy",allow_pickle=True) 
 # keys : players  -- objects : nb_simul*nb_time_steps
 
-batteries_IC_SF=np.load(name+"/data_visualize/battery_stock_simulation_IC_SF.npy",allow_pickle=True)
+batteries_IC_SF=np.load(name+"/data_visualize/battery_stock_simulation_IC_SF.npy",allow_pickle=True) 
 # keys : players (except CS)  -- objects : nb_simul*(nb_time_steps+1)
 
 batteries_CS=np.load(name+"/data_visualize/battery_stock_simulation_CS.npy",allow_pickle=True) 
@@ -35,6 +35,8 @@ scenarios_IC_SF=np.load(name+"/data_visualize/scenario_simulation_IC_SF.npy",all
 
 scenarios_CS=np.load(name+"/data_visualize/scenario_simulation_CS.npy",allow_pickle=True) 
 # keys : players (only CS)  -- objects : nb_simul*2(departures/arrivals)*nb_cars
+
+scores=np.load(name+"/data_visualize/score_simulation.npy",allow_pickle=True) 
 
 
 
@@ -49,7 +51,7 @@ grid_load=np.load(name+"/data_visualize/grid_load_simulation.npy",allow_pickle=T
 prices=np.load(name+"/data_visualize/price_simulation.npy",allow_pickle=True)
 # keys : internal/external_purchase/external_sale  -- objects : nb_simul*nb_time_steps
 
-
+real_prices=np.load(name+"/data_visualize/real_price_simulation.npy",allow_pickle=True)
 
 print(time.time()-t)
 
@@ -75,18 +77,22 @@ vis.plotCS(batteries_CS[0],'kWh','Average battery level for player','Level', 'ba
 
 #affichage scenario IC_SF"
 
-vis.plottotal(scenarios_IC_SF[0],'kW','Parameter for player','Sunlight/Demand','parametre',name)
+vis.plottotal(scenarios_IC_SF[0],'kW','Scenario for player','Sunlight/Demand','scenario',name)
 
 #affichage imbalances"
 
-vis.plot_2(imbalances[0],'%','Imbalance between demand and supply on the grid','figure_imbalances.png',name)
+vis.plot_2(imbalances[0],'%','Proportion of internal trades','figure_imbalances.png', "demand cover","supply cover",name)
 
 #affichage grid_load"
 
-vis.plot_2(grid_load[0],'kW','Internal energy balance','figure_grid_load.png',name)
+vis.plot_2(grid_load[0],'kW','Energy balance','figure_grid_load.png',"demand" , "supply" ,name)
 
 #affichage prices"
 
-vis.plot_3(prices[0],'€/kWh',"Electricity price ",'figure_prices.png',name)
+vis.plot_3(prices[0],'€/kWh',"Electricity theoritical price ",'figure_theoritical_prices.png',name)
+
+vis.plot_4(scores[0],name)
+
+vis.plot_5(real_prices[0],'€/kWh',"Electricity real price ",'figure_real_prices.png',name)
 
 print(time.time()-t)
